714892ef8c7e628442aec5ef747bac4d408efbaa
Riley-Robinson <rrtoinfinity@gmail.com>
2020-05-21 18:12:27 -0400

    Mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,40 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
-  const [movie, setMovie] = useState(null);
-  const params = useParams();
+function Movie({ addToSavedList, getMovieList }) {
+	const [movie, setMovie] = useState(null);
+	const params = useParams();
+	const { push } = useHistory();
 
-  const fetchMovie = (id) => {
-    axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then((res) => setMovie(res.data))
-      .catch((err) => console.log(err.response));
-  };
+	const fetchMovie = (id) => {
+		axios
+			.get(`http://localhost:5000/api/movies/${id}`)
+			.then((res) => setMovie(res.data))
+			.catch((err) => console.log(err.response));
+	};
 
-  const saveMovie = () => {
-    addToSavedList(movie);
-  };
+	const saveMovie = () => {
+		addToSavedList(movie);
+	};
 
-  useEffect(() => {
-    fetchMovie(params.id);
-  }, [params.id]);
+	useEffect(() => {
+		fetchMovie(params.id);
+	}, [params.id]);
 
-  if (!movie) {
-    return <div>Loading movie information...</div>;
-  }
+	if (!movie) {
+		return <div>Loading All Da movie information...</div>;
+	}
 
-  return (
-    <div className="save-wrapper">
-      <MovieCard movie={movie} />
+	const deleteMovie = (e) => {
+		e.preventDefault();
+		axios
+			.delete(`http://localhost:5000/api/movies/${params.id}`)
+			.then((res) => {
+				getMovieList();
+				push("/");
+			})
+			.catch((err) => console.log(err));
+	};
 
-      <div className="save-button" onClick={saveMovie}>
-        Save
-      </div>
-    </div>
-  );
+	return (
+		<div className="save-wrapper">
+			<MovieCard movie={movie} />
+
+			<div className="save-button" onClick={saveMovie}>
+				Save Da Movie
+			</div>
+			<button onClick={() => push(`/update-movie/${movie.id}`)}>
+				Update Da Movie
+			</button>
+			<button onClick={deleteMovie}>Delete Da Movie</button>
+		</div>
+	);
 }
 
 export default Movie;
---
+++ client/src/Movies/MovieUpdate.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
+
+const initialItem = {
+	title: "",
+	director: "",
+	metascore: "",
+	stars: [],
+};
+
+const MovieUpdate = (props) => {
+	const { push } = useHistory();
+	const [item, setItem] = useState(initialItem);
+	const { id } = useParams();
+
+	useEffect(() => {
+		axios
+			.get(`http://localhost:5000/api/movies/${id}`)
+			.then((res) => {
+				// res.data
+				setItem(res.data);
+			})
+			.catch((err) => console.log(err));
+	}, [id]);
+
+	const changeHandler = (ev) => {
+		ev.persist();
+		let value = ev.target.value;
+
+		setItem({
+			...item,
+			[ev.target.name]: value,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		axios
+			.put(`http://localhost:5000/api/movies/${id}`, item)
+			.then((res) => {
+				props.getMovieList();
+				push("/");
+			})
+			.catch((err) => console.log(err));
+	};
+
+	return (
+		<div>
+			<h2>Update Da Movie</h2>
+			<form onSubmit={handleSubmit}>
+				<label>
+					Title:
+					<input
+						type="text"
+						name="title"
+						onChange={changeHandler}
+						placeholder="title"
+						value={item.title}
+					/>
+				</label>
+				<label>
+					Director:
+					<input
+						type="text"
+						name="director"
+						onChange={changeHandler}
+						placeholder="director"
+						value={item.director}
+					/>
+				</label>
+				<label>
+					MetaScore:
+					<input
+						type="text"
+						name="metascore"
+						onChange={changeHandler}
+						placeholder="metascore"
+						value={item.metascore}
+					/>
+				</label>
+				<label>
+					Stars:
+					<input
+						type="string"
+						name="stars"
+						onChange={changeHandler}
+						placeholder="stars"
+						value={item.stars}
+					/>
+				</label>
+
+				<button className="md-button form-button">
+					{" "}
+					Update Da Movie{" "}
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default MovieUpdate;
